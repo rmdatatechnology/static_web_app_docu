d770d52527615a5462d87d3009238cc8f719abad
DKoenigshofer <koenigshofer@rmdatagroup.com>
2021-11-10 07:11:50 +0100

    set correct product name

--- src/components/MdxComponents/Products/GeoMapper/index.js
+++ src/components/MdxComponents/Products/GeoMapper/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Product from "../index"
 
 const GeoMapper = ({ children}) => {
-  return <Product currentproduct="geomapperse" children={children} />
+  return <Product currentproduct="geomapper" children={children} />
 }
 
 export default GeoMapper
