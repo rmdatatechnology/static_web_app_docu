f411c8434f72657b627bfea6e654c8c9c8448b6a
DKoenigshofer <koenigshofer@rmdatagroup.com>
2022-02-09 11:23:43 +0100

    add copy pdf

---
+++ scripts/copypdf.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var fse = require('fs-extra');
+
+var sourceDir = './convertpdf/mergehtml';
+var destDir = './static';
+
+// if folder doesn't exists create it
+if (!fs.existsSync(destDir)){
+    fs.mkdirSync(destDir, { recursive: true });
+}
+
+ fse.readdirSync(sourceDir).forEach(file => {
+    if (file.endsWith('.pdf')) {
+      console.log(`copying file ${file}`);
+      fse.copy(`${sourceDir}/${file}`, `${destDir}/${file}`)
+        .then(() => console.log('success!'))
+        .catch(err => console.error(err));
+    }
+  });
