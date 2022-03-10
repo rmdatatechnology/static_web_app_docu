896fedee1b8c1f3f33ea3d967de9514a925465fc
DKoenigshofer <koenigshofer@rmdatagroup.com>
2022-03-10 13:49:35 +0100

    fix build

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import useTranslations from '../useTranslations';
 import Navigation from '../Navigation';
 import Languages from '../Languages';
-import ButtonMenu from '../ButtonMenu';
 import Logo from '../Logo';
 import LocalizedLink from '../LocalizedLink';
 
