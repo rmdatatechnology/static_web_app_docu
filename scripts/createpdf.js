8b5c484daf357e751ae5095b6e6533cb14699df4
DKoenigshofer <koenigshofer@rmdatagroup.com>
2022-04-15 11:11:19 +0200

    adapt pdf creation

--- scripts/createpdf.js
+++ scripts/createpdf.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').execFile;
 
 var opt = function(){
-      exec('./convertpdf/mergehtml//MergeHtml.exe', ["--filepath=..\\..\\public", "--pdftool=..\\wkhtmltopdf/bin/wkhtmltopdf.exe", "--toc=..\\..\\documentation/toc", "--configpath=..\\..\\config/variables/variables.json", "--mainpage=..\\..\\public/grafik/grafik/index.html"], function(err, data) {  
+      exec('./convertpdf/mergehtml//MergeHtml.exe', ["--filepath=..\\..\\public", "--pdftool=..\\wkhtmltopdf/bin/wkhtmltopdf.exe", "--toc=..\\..\\documentation/toc", "--configpath=..\\..\\config/variables/variables.json", "--mainpage=..\\..\\public/grafik/products/index/index.html"], function(err, data) {  
         console.log(err)
         console.log(data.toString());                       
     });  
