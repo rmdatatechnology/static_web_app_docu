318499424752a32da545a31810c12d243ec8e5fb
DKoenigshofer <koenigshofer@rmdatagroup.com>
2022-04-15 11:11:19 +0200

    adapt pdf creation

--- src/components/MdxComponents/TextBlock/Info/index.js
+++ src/components/MdxComponents/TextBlock/Info/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import TextBlock from "../index"
 
 const Info = ({ children, padding, symbol }) => {
-  return <TextBlock theme="info" children={children} padding={padding} symbol="&#x1f6c8;" />
+  return <TextBlock theme="info" children={children} padding={padding} symbol="&#9432;" />
 }
 
 export default Info
