75272308e48bcd1b61aaae55c3a0e57a9661e5bf
DKoenigshofer <koenigshofer@rmdatagroup.com>
2022-05-13 11:27:19 +0200

    use correct start page for building the pdf

--- scripts/createpdf.js
+++ scripts/createpdf.js
@@ -1,9 +1,23 @@
 var exec = require('child_process').execFile;
 
-var opt = function(){
-      exec('./convertpdf/mergehtml//MergeHtml.exe', ["--filepath=..\\..\\public", "--pdftool=..\\wkhtmltopdf/bin/wkhtmltopdf.exe", "--toc=..\\..\\documentation/toc", "--configpath=..\\..\\config/variables/variables.json", "--mainpage=grafik/products/index/index.html"], function(err, data) {  
+const config = require('../config/variables/variables.json');
+
+
+var opt = function(product){
+      
+	  let mainfile = "grafik/products/index/index.html";
+	  if(product.toLowerCase() === "rmgeo")
+		  mainfile = "rmgeo/rmgeo/index.html";
+	  else if(product.toLowerCase() === "geodiscoverer")
+		  mainfile = "grafik/geodiscoverer/index/index.html";
+	   else if(product.toLowerCase() === "rmkatoffice")
+		  mainfile = "rmkatoffice/index/index.html";
+	   else if(product.toLowerCase() === "3dworx")
+		  mainfile = "3dworx/index/index.html";
+	  
+	  exec('./convertpdf/mergehtml//MergeHtml.exe', ["--filepath=..\\..\\public", "--pdftool=..\\wkhtmltopdf/bin/wkhtmltopdf.exe", "--toc=..\\..\\documentation/toc", "--configpath=..\\..\\config/variables/variables.json", "--mainpage=" + mainfile], function(err, data) {  
         console.log(err)
         console.log(data.toString());                       
     });  
 }
-opt();
+opt(config.productused);
