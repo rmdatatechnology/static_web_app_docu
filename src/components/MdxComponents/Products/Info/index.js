12b747661a270e60c6faf40144f697ddb80e3ee8
OBERWART\DKoenigshofer <koenigshofer@rmdatagroup.com>
2026-01-12 15:19:25 +0100

    add new product Info
    (cherry picked from commit 97563765851c0a88d47a0dbbf33aed6ce2ca33af)

---
+++ src/components/MdxComponents/Products/Info/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Product from "../index"
+
+const Info = ({ children }) => {
+  return <Product currentproduct="info" children={children} />
+}
+
+export default Info
